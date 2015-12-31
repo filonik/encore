d955c37a08151bd81f822552fbb6e1aede858096
unknown <daniel.filonik@qut.edu.au>
2015-12-31 23:36:23 +1000

    Reworked object serialization.

--- encore/objects.py
+++ encore/objects.py
@@ -3,7 +3,7 @@ import json
 
 import contextlib as cl
 
-from . import functions, generics, managers
+from . import accessors, functions, generics, managers, utilities
 
 
 @cl.contextmanager
@@ -15,270 +15,304 @@ def direct_access(obj):
         yield
 
 
-def wrap_attr(self, value):
-    if self._attr_cls is None:
-        return value
-    else:
-        return self._attr_cls(value, parent=self)
-
-
-def unwrap_attr(self, value):
-    try:
-        return value._data
-    except AttributeError:
-        return value
-
-
-def wrap_item(self, value):
-    if self._item_cls is None:
-        return value
-    else:
-        return self._item_cls(value, parent=self)
-
-
-def unwrap_item(self, value):
-    try:
-        return value._data
-    except AttributeError:
-        return value
-
-
-def _getattr(self, key):
-    if self._attrs is None:
-        raise AttributeError(key)
-    else:
-        try:
-            return self._wrap_attr(self._attrs[key])
-        except KeyError:
-            raise AttributeError(key)
-
-
-def _setattr(self, key, value):
-    if self._attrs is None:
-        self.__dict__[key] = value
-    else:
-        if key in self.__dict__:
-            self.__dict__[key] = value
+def combine_meta(meta, type=None):
+    if type is not None:
+        # Dynamic override of type
+        if isinstance(meta, Meta):
+            return Meta(type, attrs=meta._attrs, items=meta._items)
         else:
-            self._attrs[key] = self._unwrap_attr(value)
+            return Meta(type)
+    
+    return meta
 
 
-def _delattr(self, key):
-    if self._attrs is None:
-        del self.__dict__[key]
-    else:
-        if key in self.__dict__:
-            del self.__dict__[key]
+def dataof(obj):
+    if isinstance(obj, Object):
+        return obj._data
+    return obj
+
+
+def metaof(obj, default=None):
+    def _metaof(obj):
+        if isinstance(obj, Object):
+            return obj._meta
+        return default
+    
+    return combine_meta(_metaof(obj), typeof(obj))
+
+
+def typeof(obj, default=None):
+    if isinstance(obj, Object):
+        data = dataof(obj)
+        if isinstance(data, collections.Mapping):
+            return data.get(obj._type_key, default)
+    return default
+
+
+def attrsof(obj, default=None):
+    if isinstance(obj, Object):
+        data = dataof(obj)
+        if isinstance(data, collections.Mapping):
+            return dataof(data.get(obj._attrs_key, default))
+    return default
+
+    
+def itemsof(obj, default=None):
+    if isinstance(obj, Object):
+        data = dataof(obj)
+        if isinstance(data, collections.Mapping):
+            return dataof(data.get(obj._items_key, default))
+    return default
+
+    
+def hastype(obj):
+    return typeof(obj) is not None
+
+
+def hasattrs(obj):
+    return attrsof(obj) is not None
+
+
+def hasitems(obj):
+    return itemsof(obj) is not None
+
+    
+def type_to_string(obj):
+    if isinstance(obj, type):
+        return obj.__qualname__
+    return obj
+    
+
+def string_to_type(obj):
+    if isinstance(obj, str):
+        parts = obj.split('.')
+        module_name, class_name = parts[:-1], parts[-1]
+        if module_name:
+            module_name = ".".join(module_name)
+            m = __import__(module_name, globals(), locals(), class_name)
+            c = accessors.getattr(m, class_name)
         else:
-            del self._attrs[key]
+            c = accessors.getitem(globals(), class_name)
+        return c
+    return obj
 
 
-def _getitem(self, key):
-    return self._wrap_item(self._items[key])
+def disambiguate_meta(meta):
+    # Possibilities:
+    # 1) isinstance(meta, Meta) -> Full meta information
+    # 2) not isinstance(meta, Meta) and meta is not None -> No meta information, custom conversion function
+    # 3) not isinstance(meta, Meta) and meta is None -> No meta information, default conversion
+    
+    if isinstance(meta, Meta):
+        func_ = utilities.identity
+        meta_ = meta
+    else:
+        func_ = utilities.identity if meta is None else meta
+        meta_ = Meta()
+    
+    return meta_, func_
+
+def _reify(obj, meta=None):
+    if isinstance(obj, Object):
+        obj = obj.cast(meta)
+            
+    meta = metaof(obj, meta)
+    
+    if meta._type is not None:
+        result = meta._type()
+        setstate(result, obj)
+        return result
+    else:
+        result = dataof(obj)
+        
+        if isinstance(result, collections.Sequence) and not isinstance(result, str):
+            result = type(result)(reify(value, meta=meta.itemmeta()) for value in result)
+        
+        if isinstance(result, collections.Mapping):
+            result = type(result)((key, reify(value, meta=meta.itemmeta(key))) for key, value in result.items())
+        
+        return result
 
 
-def _setitem(self, key, value):
-    self._items[key] = self._unwrap_item(value)
+def reify(obj, meta=None):
+    meta_, func_ = disambiguate_meta(meta)
+    
+    #print("reify", obj, meta_, func_)
+    
+    return func_(_reify(obj, meta_))
 
 
-def _delitem(self, key):
-    del self._items[key]
+def defaultsetstate(self, other):
+    data = dataof(other)
+    attrs = attrsof(other)
+    items = itemsof(other)
+    
+    if attrs is None and items is None:
+        for key in data:
+            #print("setattr", key, getattr(other, key))
+            setattr(self, key, accessors.getattr(other, key))
+        
+    if attrs is not None:
+        if isinstance(attrs, collections.Sequence):
+            for key in range(len(attrs)):
+                self.append(accessors.getattr(other, key))
+        else:
+            for key in attrs:
+                #print("setattr", key, getattr(other, key))
+                setattr(self, key, accessors.getattr(other, key))
+    
+    if items is not None:
+        if isinstance(items, collections.Sequence):
+            for key in range(len(items)):
+                self.append(accessors.getitem(other, key))
+        else:
+            for key in items:
+                #print("setitem", key, getitem(data, key))
+                setitem(self, key, accessors.getitem(other, key))
 
 
-def getattrs(obj, default=None):
-    try:
-        return obj._attrs
-    except AttributeError:
-        return default
+def setstate(self, other):
+    _setstate = accessors.getattr(type(self), "__setstate__", defaultsetstate)
+    _setstate(self, other)
+
+class Meta(object):
+    def __init__(self, type=None, attrs=None, items=None):
+        super().__init__()
+        
+        self._type = string_to_type(type)
+        self._attrs = attrs
+        self._items = items
+
+    def attrmeta(self, key=None):
+        if isinstance(self._attrs, collections.Mapping):
+            return self._attrs.get(key)
+        return self._attrs
+    
+    def itemmeta(self, key=None):
+        if isinstance(self._items, collections.Mapping):
+            return self._items.get(key)
+        return self._items
+    
+    def __call__(self, data):
+        return reify(data, meta=self)
+    
+    def __repr__(self):
+        return "Meta{{{}}}".format(repr(self._type))
+    
+    def __str__(self):
+        return "Meta{{{}}}".format(", ".join(map(repr, filter(None, [self._type, self._attrs, self._items]))))
 
 
-def getitems(obj, default=None):
-    try:
-        return obj._items
-    except AttributeError:
-        return default
-
-
-@generics.generic
 class Object(object):
-    _attr_cls = generics.parameter(0)
-    _attrs_cls = generics.parameter(1, dict)
-    
-    _type_key = "type"
-    _attrs_key = "attrs"
-   
-    @property
-    def _type(self):
-        return self._data[self._type_key]
-    
-    @property
-    def _attrs(self):
-        return self._data[self._attrs_key]
-    
-    def __init__(self, data=None, parent=None):
-        super().__init__()
-        
-        with direct_access(self):
-            self._parent = parent
-            self._data = {} if data is None else data
-            self._data.setdefault(self._attrs_key, self._attrs_cls())
-    
-    _wrap_attr = wrap_attr
-    _unwrap_attr = unwrap_attr
-    
-    __getattr__ = _getattr
-    __setattr__ = _setattr
-    __delattr__ = _delattr
-    
-    def save(self):
-        return self._data
-        
-    def load(self, data):
-        self._data = data
-    
-    def __str__(self):
-        return "".join(map(str, filter(None, [self._type, self._attrs])))
-
-
-@generics.generic
-class Sequence(collections.MutableSequence):
-    _item_cls = generics.parameter(0)
-    _items_cls = generics.parameter(1, list)
-    
-    _attr_cls = generics.parameter(2)
-    _attrs_cls = generics.parameter(3, dict)
-
     _type_key = "type"
     _attrs_key = "attrs"
     _items_key = "items"
-   
-    @property
-    def _type(self):
-        return self._data[self._type_key]
     
-    @property
-    def _attrs(self):
-        return self._data[self._attrs_key]
-    
-    @property
-    def _items(self):
-        return self._data[self._items_key]
-    
-    def __init__(self, data=None, parent=None):
+    def __init__(self, data=None, meta=None):
         super().__init__()
         
         with direct_access(self):
-            self._parent = parent
-            self._data = {} if data is None else data
-            self._data.setdefault(self._attrs_key, self._attrs_cls())
-            self._data.setdefault(self._items_key, self._items_cls())
-    
-    _wrap_attr = wrap_attr
-    _unwrap_attr = unwrap_attr
-    
-    __getattr__ = _getattr
-    __setattr__ = _setattr
-    __delattr__ = _delattr
-    
-    _wrap_item = wrap_item
-    _unwrap_item = unwrap_item
-    
-    __getitem__ = _getitem
-    __setitem__ = _setitem
-    __delitem__ = _delitem
-    
-    def __contains__(self, key):
-        return key in self._items
-    
-    def __len__(self):
-        return len(self._items)
-    
-    def insert(self, key, value):
-        self._items.insert(key, self._unwrap_item(value))
-    
-    def save(self):
+            self._data = data
+            self._meta = meta
+
+    def __getstate__(self):
         return self._data
-        
-    def load(self, data):
+    
+    def __setstate__(self, data):
         self._data = data
     
-    def __str__(self):
-        return "".join(map(str, filter(None, [self._type, self._attrs, self._items])))
-
-
-@generics.generic
-class Mapping(collections.MutableMapping):
-    _item_cls = generics.parameter(0)
-    _items_cls = generics.parameter(1, dict)
-    
-    _attr_cls = generics.parameter(2)
-    _attrs_cls = generics.parameter(3, dict)
-    
-    _type_key = "type"
-    _attrs_key = "attrs"
-    _items_key = "items"
-   
-    @property
-    def _type(self):
-        return self._data[self._type_key]
-    
-    @property
-    def _attrs(self):
-        return self._data[self._attrs_key]
-    
-    @property
-    def _items(self):
-        return self._data[self._items_key]
-    
-    def __init__(self, data=None, parent=None):
-        super().__init__()
+    def __getattr__(self, key):
+        if not isinstance(self._data, collections.Mapping):
+            raise AttributeError(key)
         
-        with direct_access(self):
-            self._parent = parent
-            self._data = {} if data is None else data
-            self._data.setdefault(self._attrs_key, self._attrs_cls())
-            self._data.setdefault(self._items_key, self._items_cls())
-    
-    _wrap_attr = wrap_attr
-    _unwrap_attr = unwrap_attr
-    
-    __getattr__ = _getattr
-    __setattr__ = _setattr
-    __delattr__ = _delattr
-    
-    _wrap_item = wrap_item
-    _unwrap_item = unwrap_item
-    
-    __getitem__ = _getitem
-    __setitem__ = _setitem
-    __delitem__ = _delitem
-    
-    def __contains__(self, key):
-        return key in self._items
-    
-    def __len__(self):
-        return len(self._items)
+        meta, _ = disambiguate_meta(metaof(self))
+        attrmeta = meta.attrmeta(key)
         
+        attrs = attrsof(self, dataof(self))
+        #print("getattr", key, attrmeta)
+        return reify(accessors.getitem(attrs, key), meta=attrmeta)
+        
+    def __getitem__(self, key):
+        if not isinstance(self._data, collections.Mapping):
+            raise KeyError(key)
+        
+        meta, _ = disambiguate_meta(metaof(self))
+        itemmeta = meta.itemmeta(key)
+        
+        items = itemsof(self, dataof(self))
+        #print("getitem", key, itemmeta)
+        return reify(accessors.getitem(items, key), meta=itemmeta)
+    
     def __iter__(self):
-        return iter(self._items)
-    
-    def save(self):
-        return self._data
+        meta, _ = disambiguate_meta(metaof(self))
         
-    def load(self, data):
-        self._data = data
+        items = itemsof(self)
+        
+        if isinstance(items, collections.Sequence):
+            return iter(reify(item, meta=meta.itemmeta()) for item in items)
+        else:
+            return iter(items)
     
+    def __len__(self):
+        items = itemsof(self)
+        return len(items)
+    
+    def cast(self, meta):
+        return Object(self._data, meta)
+    
+    def __repr__(self):
+        return "Object({})".format(repr(self._data))
+        
     def __str__(self):
-        return "".join(map(str, filter(None, [self._type, self._attrs, self._items])))
+        return "Object({})".format("".join(map(str, filter(None, [typeof(self), attrsof(self), itemsof(self)]))))
 
 
-def load_json(obj, path):
-    with open(path, 'r') as file:
-        data = json.load(file, object_pairs_hook=collections.OrderedDict)
-        obj.load(data)
+class ObjectEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Object):
+            return obj.__getstate__()
+        return json.JSONEncoder.default(self, obj)
 
 
-def save_json(obj, path):
+class ObjectDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        object_hook = kwargs.pop("object_hook", utilities.identity)
+        object_pairs_hook = kwargs.pop("object_pairs_hook", utilities.identity)
+        
+        def _object_hook(data):
+            result = Object()
+            result.__setstate__(data)
+            return result
+        
+        kwargs["object_hook"] = lambda data: _object_hook(object_hook(data))
+        kwargs["object_pairs_hook"] = lambda data: _object_hook(object_pairs_hook(data))
+        
+        super().__init__(*args, **kwargs)
+
+
+def save_object(data, meta=None):
+    return json.dumps(data, cls=ObjectEncoder)
+
+
+def load_object(data, meta=None):
+    return json.loads(data, cls=ObjectDecoder, object_pairs_hook=collections.OrderedDict)
+
+
+def save(data, meta=None):
+    return save_object(data, meta=meta)
+
+
+def load(data, meta=None):
+    data = load_object(data, meta=meta)
+    return reify(data, meta=meta)
+
+    
+def save_file(data, path, meta=None):
     with open(path, 'w') as file:
-        data = obj.save()
-        json.dump(data, file)
+        json.dump(data, file, cls=ObjectEncoder)
+
+
+def load_file(path, meta=None):
+    with open(path, 'r') as file:
+        data = json.load(file, cls=ObjectDecoder, object_pairs_hook=collections.OrderedDict)
+        return reify(data, meta=meta)
